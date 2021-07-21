24dfed0f15b1dff57fb406798c811e75e7c31b5b
unknown <ikoreyvt@gmail.com>
2021-07-21 16:40:26 -0400

    styled home page and added standin get/post methods on server

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, Chat!</h1>
+    <div>
+      <header>
+        <h1 id="mainTitle">!CHAT ROOM</h1>
+        <form id="mainForm" action="/main" method="POST">
+          <h1 class="mainInputTitles">ENTER A USERNAME:</h1>
+          <input type="text" name="userName" placeholder="Username"/>
+          <h1 class="mainInputTitles">CHAT ROOM NAME:</h1>
+          <input type="text" name="chatRoom" placeholder="Chat Room"/>
+          <input type="submit" value="Connect"/>
+        </form>
+      </header>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 
@@ -8,7 +9,28 @@ const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 
 app.use(express.static(staticDir));
 
+mongoose.connect(`mongodb+srv://SamboSauce:${process.env.PASS}@cluster0.jvo7d.mongodb.net/Chat?authSource=admin&replicaSet=atlas-2ev78e-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`)
 
+const messageSchema = new mongoose.Schema({
+  time: Date,
+  author: String,
+  body: String
+})
+
+const MainMessage = mongoose.model("MainMessage", messageSchema)
+
+app.get("/mainData", async (req, res) => {
+  let mainMessages = await MainMessage.find({})
+  res.json(mainMessages)
+})
+
+app.post("/mainMessage", async(req, res) => {
+  const message = new MainMessage({
+    time: new Date(),
+    author: req.body.name,
+    body: req.body.message
+  })
+})
 
 app.listen(port, () => {
   console.log('listening on port: ' + port) 
